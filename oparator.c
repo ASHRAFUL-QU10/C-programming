501756bebc0af1440dea046efcabfbc03a2ec5b8
MUHAMMAD ASHRAFUL <106703215+ashraful-10@users.noreply.github.com>
2022-09-25 23:54:37 +0600

    +-*/

--- oparator.c
+++ oparator.c
@@ -4,8 +4,8 @@ int main() {
     int number1, number2;
     number1 = 12;
     number2 = 4;
-    printf("number1 + number2 = %d\n", number1+ number2);
-    printf("number1 + number2 = %d\n", number1+ number2);
+    printf("number1 + number2 = %d\n", number1 + number2);
+    printf("number1 - number2 = %d\n", number1 - number2);
     
     return 0;
 }
