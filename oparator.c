50d21f41320ff894069090c81a72db255c553c3e
MUHAMMAD ASHRAFUL <106703215+ashraful-10@users.noreply.github.com>
2022-09-25 23:53:24 +0600

    I added something in this code rapo

--- oparator.c
+++ oparator.c
@@ -4,9 +4,8 @@ int main() {
     int number1, number2;
     number1 = 12;
     number2 = 4;
-    printf(" %d", number1 + number2);
-    printf(" %d", number1 - number2);
-    printf(" %d", number1 * number2);
-    printf(" %d", number1 / number2);
+    printf("number1 + number2 = %d\n", number1+ number2);
+    printf("number1 + number2 = %d\n", number1+ number2);
+    
     return 0;
 }
