14bfb70db4142992493921145b22952cb276f7ea
MUHAMMAD ASHRAFUL <106703215+ashraful-10@users.noreply.github.com>
2022-09-26 12:46:00 +0600

    I added something in this code rapo

---
+++ calco.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main() {
+    int number1, number2;
+    number1 = 100;
+    number2 = 2;
+    printf("Plus Ecuation = %d\n", number1 + number2);
+    printf("MUnus Ecution = %d\n", number1 - number2);
+    printf("Ecutlo Ecution = %d\n", number1 * number2);
+    printf("Diveite Ecution =%d\n", number1 / number2);
+    return 0;
+}
