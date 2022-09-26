21fb3b0deca8459212200419d84d86446b795931
MUHAMMAD ASHRAFUL <106703215+ashraful-10@users.noreply.github.com>
2022-09-26 16:36:53 +0600

    I added char.c

--- test.c
+++ test.c
@@ -3,8 +3,8 @@
 int main() {
     char ch;
     ch = 'x';
-    printf("stor %c\n",ch);
+    printf("char %c\n",ch);
     ch = 'y';
-    printf(" stor %c\n",ch);
+    printf("char %c\n",ch);
     return 0;
     }
