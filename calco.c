98d7bf38dda0f8de733b3600ac3b3874a52dfcde
MUHAMMAD ASHRAFUL <106703215+ashraful-10@users.noreply.github.com>
2022-09-26 16:33:35 +0600

    I added calco.c

--- calco.c
+++ calco.c
@@ -4,9 +4,9 @@ int main() {
     int number1, number2;
     number1 = 100;
     number2 = 2;
-    printf("Plus Ecuation = %d\n", number1 + number2);
-    printf("MUnus Ecution = %d\n", number1 - number2);
-    printf("Ecutlo Ecution = %d\n", number1 * number2);
-    printf("Diveite Ecution = %d\n", number1 / number2);
+    printf("100 + 2 = %d\n", number1 + number2);
+    printf("100 - 2 = %d\n", number1 - number2);
+    printf("100 * 2 = %d\n", number1 * number2);
+    printf("100 / 2 = %d\n", number1 / number2);
     return 0;
 }
