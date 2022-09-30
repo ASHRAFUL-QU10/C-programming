4cddcedaca3c808839d3c3a492233fd0efaf30f7
MUHAMMAD ASHRAFUL <106703215+ashraful-10@users.noreply.github.com>
2022-09-30 08:58:55 +0600

    I added something in this code repo

--- cf.c
+++ cf.c
@@ -1,9 +1,8 @@
 # include<stdio.h>
 
 int main() {
-    double c ,f;
-    scanf("%lf", &c);
+    double c, f;
+    scanf("lf", &c);
     f = 1.8 * c + 32;
-    printf(" %lf\n", f);
+    printf("%lf\n", f);
     return 0;
-}
