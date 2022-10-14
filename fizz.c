6747efeda12f7649f5d88597585b643165675567
MUHAMAD-ASHRAFUL-Q10 <muhammadashrafulbusiness10@gmail.com>
2022-10-14 14:31:10 +0600

    I added Diveidation in this code rapo

---
+++ fizz.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    if (num % 3 == 0 &&  num % 5 == 0)
+    {
+        printf("FizzBuzz\n");
+    }
+    else if (num % 3 == 0)
+    {
+        printf("Fizz\n");
+    }
+    else if (num % 5 == 0)
+    {
+        printf("Buzz\n");
+    }
+    return 0;
+}
